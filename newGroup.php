45e086d7036a725ae7ac339d4293e046f22655bc
Rory Crispin <rcqikmail@gmail.com>
2016-10-16 20:05:16 +0100

    reformatting

---
+++ newGroup.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/newGroup.css">
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+</head>
+<body>
+
+<div class="jumbotron">
+    <h1> Create Group</h1>
+
+    <h3> Add Members</h3>
+    <div class="dynamic-form">
+
+        <a href="#" id="add">Add Rows</a><br>
+
+        <form>
+            <div class="inputs">
+                <div class="MemberRow"><input type="text" class="field name form-control shortTextBox" name="dynamic[]"
+                                              value=""
+                                              placeholder="Name"/> <input type="text"
+                                                                          class="field paypal form-control shortTextBox"
+                                                                          name="dynamic[]" value=""
+                                                                          placeholder="Paypal.me link"/></div>
+            </div>
+            <input name="submit" type="button" class="submit btn btn-success inline" value="Submit">
+        </form>
+    </div>
+
+    <script>
+        $(document).ready(function () {
+
+
+            $('#add').click(function () {
+                $('<div class ="MemberRow"><input type="text" class="field name form-control shortTextBox" name="dynamic[]" value="" ' +
+                    'placeholder="Name"  /> <input type="text" class="field paypal form-control shortTextBox" name="dynamic[]" value="" placeholder="Paypal.me link"  /> </div>    ').fadeIn('slow').appendTo('.inputs');
+
+            });
+
+
+            $('.submit').click(function () {
+
+                var people = [];
+                $.each($('.MemberRow'), function () {
+                    console.log(this);
+                    var nameBox = this.getElementsByClassName("name");
+                    var paypalBox = this.getElementsByClassName("paypal");
+                    people.push({name: $(nameBox).val(), paypal: $(paypalBox).val()});
+
+                });
+
+                console.log(people);
+
+                console.log(JSON.stringify(people));
+
+                $.ajax({
+                    url: "registerGroup.php",
+                    data: "people=" + JSON.stringify(people),
+                    type: 'post',
+                    success: function (people) {
+                        alert(people);
+                    }
+                });
+
+
+                return false;
+
+            });
+
+        });
+
+    </script>
+</div>
+</body>
+
+
+</html>
