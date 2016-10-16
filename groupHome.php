93d4ecd88be9808a2ec55d6c3277f469653b3403
Tudor Sandulescu <sekaru@sleepystudios.com>
2016-10-16 21:24:16 +0100

    Fixed group home, pin setting

--- groupHome.php
+++ groupHome.php
@@ -5,8 +5,6 @@
     <link rel="stylesheet" href="css/index.css" >
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/countUp.min.js"></script>
-
 
     <meta charset="UTF-8">
     <title>KittyJar - Who are you?</title>
@@ -24,14 +22,64 @@
 
     <?php
         $groupID = 1;
-        $sql = "SELECT name FROM User WHERE groupID = " . $groupID . " ORDER BY name";
+        $sql = "SELECT * FROM User WHERE groupID = " . $groupID . " ORDER BY name";
         $result = $conn->query($sql);
         if($result->num_rows > 0) {
             while ($row = $result->fetch_array()) {
-                echo("<button type='button' class='btn btn-primary who-btn'>" . $row['name'] . "</button><br/>");
+                if(empty($row['pin'])) {
+                    echo("<button type='button' class='btn btn-primary who-btn' data-toggle='modal' data-target='#setPinModal'>" . $row['name'] . "</button><br/>");
+                } else {
+                    echo("<button type='button' class='btn btn-primary who-btn' data-toggle='modal' data-target='#loginModal'>" . $row['name'] . "</button><br/>");
+                }
             }
         }
     ?>
+
+    <div id="setPinModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Login</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Set your PIN: you'll use this to login to your house.</p>
+                    <div class="input-group">
+                        <input required type="text" id='userPin' class="form-control">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div id="loginModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Login</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Enter your PIN:</p>
+                    <div class="input-group">
+                        <input required type="text" id='userPin' class="form-control">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </div>
 
 </body>
