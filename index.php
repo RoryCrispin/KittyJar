df14c1239310b68aba482575091833ef6019b191
Rory Crispin <rcqikmail@gmail.com>
2016-10-16 20:12:43 +0100

    submit button href

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="css/bootstrap.min.css" >
-    <link rel="stylesheet" href="css/index.css" >
-    <link rel="stylesheet" href="css/main.css" >
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/main.css">
 
     <script src="js/bootstrap.min.js"></script>
 
@@ -23,12 +23,14 @@
 
     <h6>Already got a house?</h6>
     <form action="groupHome.html" method="get">
-        <input type="text" name="groupCode" class="form-control grpCodeText shortTextBox" placeholder="Group code" aria-describedby="basic-addon1">
+        <input type="text" name="groupCode" class="form-control grpCodeText shortTextBox" placeholder="Group code"
+               aria-describedby="basic-addon1">
         <button type="submit" class="btn btn-primary grpCodeButton">Enter!</button>
     </form>
-<br>
-    <button type="button" class="btn btn-success">Register Group</button>
-
+    <br>
+    <form action="newGroup.php">
+        <button type="Submit" class="btn btn-success">Register Group</button>
+    </form>
 
 </div>
 
