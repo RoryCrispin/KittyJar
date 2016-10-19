114b1145248b3246b12dabd8e4123701d3478c45
sekaru <sekaru@sleepystudios.com>
2016-10-19 17:19:27 +0100

    View debts updates

--- viewDebts.php
+++ viewDebts.php
@@ -23,19 +23,35 @@
     <br/>
 
     <ul class="list-group">
-        <li class="list-group-item list-group-item-danger">Unpaid Debt 1 <button type="button" class="btn btn-primary btn-sm pay-btn">Pay now</button></li>
-        <li class="list-group-item list-group-item-danger">Unpaid Debt 2</li>
-        <li class="list-group-item list-group-item-danger">Unpaid Debt 3</li>
+        <li class="list-group-item list-group-item-danger">Unpaid Debt 1
+            <button type="button" class="btn btn-primary btn-sm pay-btn">Pay now</button>
+        </li>
+        <li class="list-group-item list-group-item-danger">Unpaid Debt 2
+            <button type="button" class="btn btn-primary btn-sm pay-btn">Pay now</button>
+        </li>
+        <li class="list-group-item list-group-item-danger">Unpaid Debt 3
+            <button type="button" class="btn btn-primary btn-sm pay-btn">Pay now</button>
+        </li>
     </ul>
     <br/>
     <ul class="list-group">
-        <li class="list-group-item list-group-item-warning">Waiting for Approval 1</li>
-        <li class="list-group-item list-group-item-warning">Waiting for Approval 2</li>
-        <li class="list-group-item list-group-item-warning">Waiting for Approval 3</li>
+        <li class="list-group-item list-group-item-warning">Waiting for Approval 1
+            <span class="date">Paid on 12/11/16 (waiting for approval)</span>
+        </li>
+        <li class="list-group-item list-group-item-warning">Waiting for Approval 2
+            <span class="date">Paid on 12/11/16 (waiting for approval)</span>
+        </li>
+        <li class="list-group-item list-group-item-warning">Waiting for Approval 3
+            <span class="date">Paid on 12/11/16 (waiting for approval)</span>
+        </li>
     </ul>
     <br/>
+
+    <h4>History</h4>
     <ul class="list-group">
-        <li class="list-group-item list-group-item-success">Paid Debt 1</li>
+        <li class="list-group-item list-group-item-success">Paid Debt 1
+            <span class="date">Paid on 12/11/16</span>
+        </li>
     </ul>
 </div>
 
