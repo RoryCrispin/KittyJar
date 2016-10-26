5dd3964e44f8569fbaea335c0dc9590cfb097e0d
Tudor Sandulescu <sekaru@sleepystudios.com>
2016-10-26 14:47:26 +0100

    Libs.php

---
+++ libs.php
@@ -0,0 +1,4 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/validatePin.js"></script>
+<script src="js/js.cookie.js"></script>
